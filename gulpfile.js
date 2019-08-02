0830d16530d57ee8aff80372a3dab7b2bf2908e4
cycjimmy <cycjimmy@gmail.com>
2019-08-02 16:04:00 +0800

    v0.5.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,16 @@
 const
   gulp = require('gulp')
   , webpackTasks = require('./gulp/webpack')
-  , svgstoreTasks = require('./gulp/svgstore')
   , deployTasks = require('./gulp/deploy')
 ;
 
 // srcPaths
 global.srcPaths = {
-  icons: {                             // icons
-    from: 'static/icons/',
-    to: 'static/images/icons/',
-  },
   build: 'build',                     // Eventually export
   node_modules: "node_modules"         // Node dependent packages
 };
 
 // task
-gulp.task('svgstore', svgstoreTasks.svgstore);
 gulp.task('build', webpackTasks.packBuild);
 gulp.task('build:watch', webpackTasks.packBuildWatch);
 gulp.task('build:test_server:watch', webpackTasks.packBuildTestServerWatch);
